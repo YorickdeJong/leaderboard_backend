1645587d713ed05cae132abf8264aa4234accece
Arthike Kandasamy <arthike36@gmail.com>
2023-11-12 18:55:00 +0100

    second commit

--- src/main/java/com/example/java_test/controller/GoogleController.java
+++ src/main/java/com/example/java_test/controller/GoogleController.java
@@ -11,13 +11,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.Date;
 import java.util.Map;
 
 @RequestMapping("/api/auth/google")
-
+@RestController
 public class GoogleController {
 
     @Value("${spring.security.oauth2.client.registration.google.client-id}")
